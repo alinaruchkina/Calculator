a5a35d3d6066f00910358dd8be4a81271886ee2c
NikRuchkin1 <bear419000@gmail.com>
2021-11-08 23:59:13 +0300

    added themes for calculator

--- js/script.js
+++ js/script.js
@@ -51,3 +51,66 @@ function setPercent() {
   outputExample = outputExample.replace('/100*', '%'); //осуществляет замену только первого совпадения.
   document.querySelector('.content__example').innerHTML = outputExample;
 }
+
+function changeThemeOnWhite() {
+  let background = document.querySelector('#background');
+  background.style.background = `linear-gradient(#EBF6FF, #C6E3FF)`;
+
+  let equally = document.querySelector('#equally');
+  equally.style.cssText = `color: #6B7486;
+  background-color: #B1D1FF`;
+
+  let numbers = document.querySelectorAll('#numbers');
+  numbers.forEach((item) => {
+    item.style.color = `#717B8D`;
+  });
+
+  let arifm = document.querySelectorAll('#arithmetic_operation');
+  arifm.forEach((item) => {
+    item.style.cssText = `color: #717B8D;
+    background-color: #E5F3FF`;
+  });
+}
+changeThemeOnWhite();
+
+function changeThemeOnBlue() {
+  let background = document.querySelector('#background');
+  background.style.background = `linear-gradient(#00CFFF, #0186FF)`;
+
+  let equally = document.querySelector('#equally');
+  equally.style.cssText = `color: #DEF6FF;
+  background-color: #007FFF`;
+
+  let numbers = document.querySelectorAll('#numbers');
+  numbers.forEach((item) => {
+    item.style.color = `#D7F3FF`;
+  });
+
+  let arifm = document.querySelectorAll('#arithmetic_operation');
+  arifm.forEach((item) => {
+    item.style.cssText = `color: #DEF6FF;
+    background-color: #30BDFF`;
+  });
+}
+// changeThemeOnBlue();
+
+function changeThemeOnBlack() {
+  let background = document.querySelector('#background');
+  background.style.background = `linear-gradient(#2B3237, #171C20)`;
+
+  let equally = document.querySelector('#equally');
+  equally.style.cssText = `color: #B1B1B2;
+    background-color: #303437`;
+
+  let numbers = document.querySelectorAll('#numbers');
+  numbers.forEach((item) => {
+    item.style.color = `#B1B1B2`;
+  });
+
+  let arifm = document.querySelectorAll('#arithmetic_operation');
+  arifm.forEach((item) => {
+    item.style.cssText = `color: #B5B5B5;
+    background-color: #3B4147`;
+  });
+}
+// changeThemeOnBlack();
